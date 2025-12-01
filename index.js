c33debe372b07f46db27ce37df8b023fe1aa31d1
themostfluffy <Fuzzyfox9876@gmail.com>
2025-12-01 20:43:06 +0000

    lol

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function setup() {
   createCanvas(400, 400);
 }
 
+function draw() { 
 
-  background(200);
   fill(100, 150, 255);
-  ellipse(200, 200, 100, 100);
+  ellipse(200, 200, randomNumber(0, 100), randomNumber(0, 100));
 
+}
