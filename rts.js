f092eadfa434257812a043258c923fe6e9d8d2c7
themostfluffy <Fuzzyfox9876@gmail.com>
2025-12-03 15:45:47 +0000

    iuhsdfhuibsdrfihj

---
+++ rts.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(1500, 715);
+}
+
+function draw() {
+  background(0, 0, 0);
+  fill(200, 200, 200);
+  noStroke(0, 0, 0);
+  ellipse(width / 2, height / 2, 200, 200);
+  fill(0, 0, 0);
+}
+function mousePressed() {
+  let d = dist(mouseX, mouseY, width / 2, height / 2);
+  if (d < 100) {
+    let games = [
+        "Stellaris",
+        "Age of Empires Iv",
+        "frostpunk",
+        "civilization vi",
+
+    ];
+    let randomIndex = floor(random(games.length));
+    let selectedGame = games[randomIndex];
+    alert("You should play: " + selectedGame);
+  } else {
+    alert("Click on the wheel to get a game suggestion!");
+  }
+}
