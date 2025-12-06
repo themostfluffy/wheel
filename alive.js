96df1236f48d4790437e5a280840b33656646ccd
themostfluffy <Fuzzyfox9876@gmail.com>
2025-12-06 04:46:03 +0000

    yes

--- alive.js
+++ alive.js
@@ -13,24 +13,18 @@ function mousePressed() {
   let d = dist(mouseX, mouseY, width / 2, height / 2);
   if (d < 200) {
     let games = [
-        "Stellaris",
-        "Age of Empires Iv",
-        "frostpunk",
-        "civilization vi",
-        "rimworld",
-        "anno 1800",
+        "subnautica",
+        "subnautica below zero",
         "project zomboid",
-        "total war: warhammer 3",
-        "total war: warhammer 2",
-        "total war: warhammer 1",
-
-        
+        "the forest",
+        "stranded deep",
+        "rimworld",
 
     ];
     let randomIndex = floor(random(games.length));
     let selectedGame = games[randomIndex];
     alert("You should play: " + selectedGame);
   } else {
-    alert("please click the circle!!!!!!!!!");
+    alert("Click the button you idiot!");
   }
 }
